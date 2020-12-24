09a3b722dce3a8b82735ca798d379f4e2c4e5087
Sarvesh-Sathish <34663815+Sarvesh-Sathish@users.noreply.github.com>
2020-12-24 00:37:04 -0600

    Initial Commit

--- test2.cpp
+++ test2.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
+//methods go here
+int add(int a, int b);
 
+//main method
 int main()
 {
-    cout << "ji";
+    int a;
+    int b;
+    cout << "Enter your first number: "; cin >> a;
+    cout << "Enter your second number: "; cin >> b;
+    cout << add(a,b) << endl;
+}
+
+//add method
+int add(int a, int b)
+{
+    return a+b;
+
 }
