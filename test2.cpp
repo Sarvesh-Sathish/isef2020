42ef67e862d55f0f081e2c1fd4d023af23cf2909
SARVESH SATHISH <sarveshsathish.2004@gmail.com>
2020-12-24 07:02:41 +0000

    mom

--- test2.cpp
+++ test2.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
 //methods go here
-int add(int a, int b);
+long add(long a, long b);
 
 //main method
 int main()
 {
-    int a;
-    int b;
+    long a;
+    long b;
     cout << "Enter your first number: "; cin >> a;
     cout << "Enter your second number: "; cin >> b;
     cout << add(a,b) << endl;
 }
 
 //add method
-int add(int a, int b)
+long add(long a, long b)
 {
     return a+b;
 //sfd
